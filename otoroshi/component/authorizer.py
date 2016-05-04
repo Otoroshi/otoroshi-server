c79d0e57440b127737f6baf67353edb5fd1ea9bc
Romain E Silva <contact@romainesilva.fr>
2016-05-04 18:51:09 +0200

    Add an authorizer to allow actuators to register procedure

--- otoroshi/component/authorizer.py
+++ otoroshi/component/authorizer.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
+import re
+
 from autobahn import wamp
-from autobahn.wamp.exception import ApplicationError
 
 from .base import Component
-from otoroshi.model import Account
+from otoroshi.model import Account, Actuator
 
 
 class AuthorizerComponent(Component):
-    @wamp.register(u'com.betamachine.authorizer.actuator_manager')
-    def authorize_actuator_manager(self, session, uri, action):
-        print("Actuator Manager ask for authorize : {}, {}, {}".format(
-            session, uri, action))
-        return True
+    @wamp.register(u'com.betamachine.authorizer.actuator')
+    def authorize_actuator(self, session, uri, action):
+        account = self._session.query(Account).filter(
+            Account.username == session['authid']).one()
+        actuator = self._session.query(Actuator).filter(
+            Actuator.account_username == account.username).one()
+
+        has_right = re.findall(
+            r'^com\.betamachine\.actuator\.{}\.(?:.+)\.(high|low|toggle)$'.format(
+                actuator.account_username), uri)
+        if len(has_right) > 0:
+            print "Actuator {} successfully registered {} procedure".format(
+                actuator, uri)
+            return True
+
+        print "Actuator {} failed to register {} procedure".format(actuator, uri)
+        return False
