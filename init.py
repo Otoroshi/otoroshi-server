8993ae84a5eb6bfa24e191fbc11b5984f3cd4294
Romain E Silva <contact@romainesilva.fr>
2016-05-04 18:44:18 +0200

    Remove actuator manager model to just keep actuators
    At the begin actuator was considered as channel of actuator manager
    which are used as a device managing multiple actuators. To keep things
    simple, actuator and actuator manager has been merged in one model.
    
    Actuator will now register procedure to the router as follow :
    
    com.betamachine.actuator.NAME.CHANNEL.LOW|HIGH|TOGGLE
    
    where name refer as the actuator name, channel as a string which for
    example refer as the GPIO pin for the ``actuator-rpig`` project and an
    action that can be LOW, HIGH or TOGGLE.

--- init.py
+++ init.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import uuid
 
 from otoroshi.config import Config
 from otoroshi.db import Db
-from otoroshi.model import Account
+from otoroshi.model import Account, Actuator, Card, Listener
 
 
 if __name__ == '__main__':
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     accounts = (
         ("admin", "admin", "admin"),
         ("listener", "listener", "listener"),
-        ("actuator", "actuator", "actuator_manager"),
+        ("actuator", "actuator", "actuator"),
     )
 
     for i, account in enumerate(accounts):
@@ -28,4 +29,40 @@ if __name__ == '__main__':
             role=account[2]
         ))
         database.session.commit()
+
+    actuators = (
+        ("actuator_1", "1", "actuator"),
+    )
+    for i, actuator in enumerate(actuators):
+        database.session.add(Actuator(
+            id=actuator[0],
+            name=actuator[1],
+            account_username=actuator[2]
+        ))
+        database.session.commit()
+
+    listeners = (
+        (str(uuid.uuid4()), "listener_1", "actuator_1", "listener", "1"),
+    )
+    for i, listener in enumerate(listeners):
+        database.session.add(Listener(
+            id=listener[0],
+            name=listener[1],
+            actuator_id=listener[2],
+            account_username=listener[3],
+            actuator_channel=listener[4],
+        ))
+        database.session.commit()
+
+    cards = (
+        ("card_1", "Romain", "Vorex"),
+    )
+    for i, card in enumerate(cards):
+        database.session.add(Card(
+            id=card[0],
+            shortname=card[1],
+            fullname=card[2]
+        ))
+        database.session.commit()
+
     print "Database created"
--- otoroshi/model.py
+++ otoroshi/model.py
@@ -12,7 +12,7 @@ class Account(BaseModel):
     username = Column(String(32), primary_key=True, nullable=False)
     secret = Column(String, nullable=False)
     role = Column(
-        Enum('admin', 'listener', 'actuator_manager'), nullable=False)
+        Enum('admin', 'listener', 'actuator'), nullable=False)
 
     def __repr__(self):
         return "<Account('%s', '%s')>" % (self.username, self.role)
@@ -37,25 +37,13 @@ class Listener(BaseModel):
     actuator_id = Column(
         String(32), ForeignKey('actuators.id'), nullable=False)
     actuator = relationship('Actuator')
+    account_username = Column(
+        String(32), ForeignKey('accounts.username'), nullable=False)
+    account = relationship('Account')
+    actuator_channel = Column(String(32), nullable=False)
 
     def __repr__(self):
         return "<Listener('%s', '%s')>" % (self.id, self.name)
-
-    def to_json(self):
-        return json.dumps({
-            "id": self.id,
-            "name": self.name
-        })
-
-
-class ActuatorManager(BaseModel):
-    __tablename__ = 'actuator_managers'
-
-    id = Column(String(32), primary_key=True)
-    name = Column(String)
-
-    def __repr__(self):
-        return "<ActuatorManager('%s', '%s')>" % (self.id, self.name)
 
     def to_json(self):
         return json.dumps({
@@ -69,11 +57,15 @@ class Actuator(BaseModel):
 
     id = Column(String(32), primary_key=True)
     name = Column(String)
-    actuator_manager_id = Column(
-        String(32), ForeignKey('actuator_managers.id'),
-        nullable=False)
-    actuator_manager = relationship('ActuatorManager')
+    account_username = Column(
+        String(32), ForeignKey('accounts.username'), nullable=False)
+    account = relationship('Account')
 
     def __repr__(self):
-        return "Actuator('%s', '%s', '%s')>" % (
-            self.id, self.name, self.actuator_manager.name)
+        return "<Actuator('%s', '%s')>" % (self.id, self.name)
+
+    def to_json(self):
+        return json.dumps({
+            "id": self.id,
+            "name": self.name
+        })
